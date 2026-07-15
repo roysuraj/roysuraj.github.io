a432f0adbbf9e1f7ee8d2e4bcf3919c7d5445e6d
SurajRoy-Inland <suraj.r@karex.in>
2026-07-15 16:49:26 +0530

    kolkata music updated

--- src/features/sounds/definitions/music.ts
+++ src/features/sounds/definitions/music.ts
@@ -1,13 +1,10 @@
 import { Howl } from "howler";
 
-import trackLuci from "../../../assets/music/luci.ogg";
-import trackAbout from "../../../assets/music/ambient-pads.ogg";
-
 import type { MusicTrack } from "../types";
 
 export const musicTracks = {
-  luci: new Howl({ src: [trackLuci], loop: true, volume: 0, preload: true }),
-  about: new Howl({ src: [trackAbout], loop: true, volume: 0, preload: true }),
+  luci: new Howl({ src: ["/sounds/music_keno.ogg"], loop: true, volume: 0, preload: true }),
+  about: new Howl({ src: ["/sounds/music_kolkata.ogg"], loop: true, volume: 0, preload: true }),
 } as const;
 
 export const BASE_VOLUMES = {
